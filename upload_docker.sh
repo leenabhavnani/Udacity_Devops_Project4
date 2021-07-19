c149c92b893f75c0925136b43f22373ea57e86a6
Ubuntu <ubuntu@ip-172-31-24-217.us-west-2.compute.internal>
2021-07-19 17:47:33 +0000

    docker changes

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=leenabhavnani/udacity_devops_project4
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+export DOCKER_ID_USER="leenabhavnani"
+docker login
+docker tag leenabhavnani/udacity_devops_project4 $DOCKER_ID_USER/udacity_devops_project4 
 # Step 3:
 # Push image to a docker repository
+docker push $DOCKER_ID_USER/udacity_devops_project4 
